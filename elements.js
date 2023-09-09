e653ddbe00ce2e5ef410cee66dbca40adbfdc3cb
Simphiwe Mbuli <118311327+Simphiwe06@users.noreply.github.com>
2023-09-09 03:13:46 +0200

    Added more code to meet user stories

---
+++ elements.js
@@ -0,0 +1,34 @@
+
+export const body = document.querySelector('body')
+
+
+
+export const settingOverlay = document.querySelector('[data-settings-overlay]')
+ export const settingCancelBtn = document.querySelector('[data-settings-cancel]');
+export const  settingForm =document.querySelector('[data-settings-form]')
+export const settingTheme = document.querySelector('[data-settings-theme]') 
+export const themeBtn = document.querySelector('[data-settings-save]')
+export const themeOpt = document.querySelector('.option')
+
+export const settingHeaderBtn = document.querySelector('[data-header-settings]')
+export const searchHeaderBtn = document.querySelector('[data-header-search]')
+export const searchCancelBtn = document.querySelector('[data-search-cancel]');
+export const searchForm = document.querySelector('[data-search-form]')
+export const searchOverlay = document.querySelector('[data-search-overlay]');
+export const searchTitle = document.querySelector('[data-seach-title]')
+
+export const dataListBtn = document.querySelector('[data-list-button]')
+export const dataListItem = document.querySelector('[data-list-items]');
+export const listCloseBtn = document.querySelector('[data-list-close]')
+export const listMessage = document.querySelector('[data-list-message]')
+export const listActive = document.querySelector('[data-list-active]');
+export const listBlur = document.querySelector('[data-list-blur]')
+export const listTitle = document.querySelector('[data-list-title]');
+export const listImg = document.querySelector('[data-list-image]');
+
+export const listSubtitle = document.querySelector('[data-list-subtitle]')
+export const listDescription = document.querySelector('[data-list-description]')
+
+export const searchAuthor = document.querySelector('[data-search-authors]')
+export const searchGenre = document.querySelector('[data-search-genres]');
+
